38cbdf28fe9aad33346ecd3e32a2cd3417b7427b
In Joong <injoong01@hotmail.com>
2021-02-27 18:55:41 -0600

    Added a function to create training and testing compressed dataset from the original compressed dataset

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,6 +1,7 @@
 import skimage.transform
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 kanji = 881
 rows = 48
@@ -15,7 +16,8 @@ train_images = np.zeros([kanji * 160, rows, cols], dtype=np.float32)
 arr = np.arange(kanji)
 train_labels = np.repeat(arr, 160)
 
-# 4 characters were actually hiragana, so delete these 4 extras
+# Decompressing
+print("Decompressing...")
 for i in range(kanji * 160):
 	if int(i/160) != 88 and int(i/160) != 219 and int(i/160) != 349 and int(i/160) != 457:
 		if int(i/160) < 88:
@@ -25,12 +27,26 @@ for i in range(kanji * 160):
 		if int(i/160) > 219 and int(i/160) < 349:
 			train_images[i-320] = skimage.transform.resize(kan[i], (rows, cols))
 		if int(i/160) > 349 and int(i/160) < 457:
+			pass
 		if int(i/160) > 457:
 			train_images[i-640] = skimage.transform.resize(kan[i], (rows, cols))
       
 train_images, test_images, train_labels, test_labels = train_test_split(train_images, train_labels, test_size=0.2)
+print("Finished decompressing")
 
+print("Initializating compresion...")
 np.savez_compressed("kanji_train_images.npz", train_images)
 np.savez_compressed("kanji_train_labels.npz", train_labels)
 np.savez_compressed("kanji_test_images.npz", test_images)
 np.savez_compressed("kanji_test_labels.npz", test_labels)
+print("Finished compresion")
+
+print("Starting visualization")
+plt.figure(figsize=(6,6)).patch.set_facecolor('#000000')
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+plt.show()
